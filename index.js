bb992da24814e92115d8f84a6ecb8516cb926b3e
Aaron <aaronf.code@gmail.com>
2020-02-20 08:12:23 -0500

    added ejs and fetch

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const app = express();
 
 
-app.get('/', (req,res) =>{
-   
-})
+let city = 'Toronto'
+const url= `http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=236bc05c1ab1689a1783fb805b8db2b8`
+
+
+const getData = async url => {
+    try{
+        const response = await fetch(url)
+        const json = await response.json()
+        console.log(json)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+getData(url);
