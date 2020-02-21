58ca1c5c61253d6465d6cfff5949d5f9268c0a95
Aaron <aaronf.code@gmail.com>
2020-02-21 13:31:32 -0500

    GET method is used on form to display info from API

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const express = require('express')
 const fetch = require('node-fetch')
 const app = express();
+const bodyParser = require('body-parser')
 
-
-let city = 'Toronto'
-const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=236bc05c1ab1689a1783fb805b8db2b8`
-
-
-app.use(express.static('css'))
-
-app.get('/', (req,res) => {
-    fetch(url)
-    .then((res) => res.json())
-    .then((data)=> {
-        // console.log(data)
-      let weather = {
-        city: city,
-        temperature: data.main.temp,
-        description: data.weather[0].description,
-        icon: data.weather[0].icon
-    }
-    let weather_data = {weather : weather}
-    res.render('weather', weather_data)
-    })
-   
-    // console.log(getData(url))
-})
+// Middleware 
 app.set("view engine", 'ejs')
+app.use(express.static('css'))
+app.use(bodyParser.urlencoded({extended: true}))
 
+
+// Routes
+app.get('/', (req,res) => {
+  const city = req.query.city_name
+  const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=236bc05c1ab1689a1783fb805b8db2b8`
+  fetch(url)
+  .then((res) => res.json())
+  .then((data)=> {
+      // console.log(data)
+    let weather = {
+      city: req.query.city_name,
+      temperature: Math.round(data.main.temp),
+      description: data.weather[0].description,
+      icon: data.weather[0].icon
+  }
+  let weather_data = {weather : weather}
+  res.render('weather', weather_data)
+  })
+})
+
+// Declaring PORT
 const port = process.env.PORT || 3000; // PORT is an environment variable that will be set by the process obj
+// Set up listener
 app.listen(port, () => {console.log(`Listening on ${port}...`)})
